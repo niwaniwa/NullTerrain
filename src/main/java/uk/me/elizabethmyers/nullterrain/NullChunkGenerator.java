f32438df4ea87a9b68e4e9dc992390a9e3c6277e
niwaniwa <konya.kokekokko@gmail.com>
2019-12-20 23:13:00 +0900

    Updated for 1.13.2

--- src/main/java/uk/me/elizabethmyers/nullterrain/NullChunkGenerator.java
+++ src/main/java/uk/me/elizabethmyers/nullterrain/NullChunkGenerator.java
@@ -1,21 +1,25 @@
 package uk.me.elizabethmyers.nullterrain;
 
 import java.util.Random;
+
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.World;
 
-public class NullChunkGenerator extends ChunkGenerator
-{
-	public byte[] generate(World world, Random random, int cx, int cz)
-	{
-		return new byte[65536];
-	}
+public class NullChunkGenerator extends ChunkGenerator {
 
+    // set default spawn
+    @Override
+    public Location getFixedSpawnLocation(World world, Random random) {
+        // Sane default --Elizabeth
+        return new Location(world, 0, 64, 0);
+    }
+
+    // added new method
 	@Override
-	public Location getFixedSpawnLocation(World world, Random random)
-	{
-		// Sane default --Elizabeth
-		return new Location(world, 0, 64, 0);
-	}
+    public ChunkData generateChunkData(World world, Random random, int x, int z, BiomeGrid Biome) {
+        return createChunkData(world);
+    }
 }
--- src/main/java/uk/me/elizabethmyers/nullterrain/NullTerrain.java
+++ src/main/java/uk/me/elizabethmyers/nullterrain/NullTerrain.java
@@ -1,33 +1,29 @@
 package uk.me.elizabethmyers.nullterrain;
 
 import java.util.logging.Logger;
-import org.bukkit.Bukkit;
+
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class NullTerrain extends JavaPlugin
-{
-	private static Logger log = Logger.getLogger("Minecraft");
+public class NullTerrain extends JavaPlugin {
+    private static Logger log = Logger.getLogger("Minecraft");
 
-	public void onDisable()
-	{
-		PluginDescriptionFile desc = this.getDescription();
+    public void onDisable() {
+        PluginDescriptionFile desc = this.getDescription();
 
-		log.info(desc.getName() + " version " + desc.getVersion() + " is shutting down!");
-	}
+        log.info(desc.getName() + " version " + desc.getVersion() + " is shutting down!");
+    }
 
-	public void onEnable()
-	{
-		PluginDescriptionFile desc = this.getDescription();
+    public void onEnable() {
+        PluginDescriptionFile desc = this.getDescription();
 
-		log.info(desc.getName() + " version " + desc.getVersion() + " is enabled!");
-	}
+        log.info(desc.getName() + " version " + desc.getVersion() + " is enabled!");
+    }
 
-	@Override
-	public ChunkGenerator getDefaultWorldGenerator(String worldName, String id)
-	{
-		return new NullChunkGenerator();
-	}
+    @Override
+    public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
+        return new NullChunkGenerator();
+    }
 }
 
